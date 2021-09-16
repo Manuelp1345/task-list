7ff488c1dac8ba0b1bb9e5a85e7500def50ef2f2
Manuelp1345 <manuelp1345@gmail.com>
2021-09-16 11:54:08 -0400

    Fix Bug login

--- src/auth/index.js
+++ src/auth/index.js
@@ -3,7 +3,7 @@ import { Auth, AUTH_STRATEGIES } from "@8base/auth";
 const domain =
   "https://6141333fe0de77000913e554.auth.us-east-1.amazoncognito.com";
 const clientId = "m8urc7ggf6tdo1klr3qahnvpu";
-const logoutRedirectUri = `https://tasklist-manuel1345.netlify.app/login`;
+const logoutRedirectUri = `https://tasklist-manuel1345.netlify.app/`;
 const redirectUri = `https://tasklist-manuel1345.netlify.app/`;
 
 export const login = Auth.createClient(
